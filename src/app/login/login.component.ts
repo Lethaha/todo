5caa126f1aed932db21a4fa4169f2a555ada2538
Lethaha <lethha.thamae@gmail.com>
2022-06-15 11:16:59 +0200

    include validations for password and username-hardcoded

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,13 +9,22 @@ export class LoginComponent implements OnInit {
 
   userName = "Teboho"
   password = ""
+  invalidCredentials = "invalid credentials"
+  isInvalidCredentials = false
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   handleLogin(){
-    console.log(this.userName)
+      if(this.userName==="Teboho" && this.password==="123"){
+        this.isInvalidCredentials =false
+
+      }
+      else{
+        this.isInvalidCredentials =true
+      }
   }
 
 }
