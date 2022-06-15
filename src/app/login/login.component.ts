13de48d96bb134c4a0fc2190502bda373695862d
Lethaha <lethha.thamae@gmail.com>
2022-06-15 10:46:54 +0200

    used ngmodel for 2 way data binding in Login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userName = "Teboho"
+  password = ""
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(){
+    console.log(this.userName)
   }
 
 }
